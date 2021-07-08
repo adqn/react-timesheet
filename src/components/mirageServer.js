2bfd0fec7fe4c56a03744553a526dcf6282e6f24
adqn <57331120+adqn@users.noreply.github.com>
2021-07-07 17:20:41 -0700

    some mock timesheet data

--- src/components/mirageServer.js
+++ src/components/mirageServer.js
@@ -2,12 +2,33 @@ import { createServer } from "miragejs";
 
 // Maybe useful:
 // https://github.com/miragejs/examples/blob/master/react-typescript/src/mirage/index.ts
+
 export function makeServer() {
-    return createServer({    
-        routes() {
-            this.get("/api/test", () => ({
-                stuff: [{thing: "This has stuff"}],
-            }))          
-        },
-    });
+  return createServer({
+    routes() {
+      this.get("/api/test", () => ({
+        stuff: [{ thing: "This has stuff" }],
+        daily: [
+          {
+            user: "Test User",
+            project: "React timesheet",
+            progress: "1%",
+            date: "7-7-2021"
+          },
+          {
+            user: "Test User",
+            project: "React timesheet",
+            progress: "1.4%",
+            date: "7-8-2021"
+          },
+          {
+            user: "Test User",
+            project: "React timesheet",
+            progress: "1.6%",
+            date: "7-9-2021"
+          }
+        ]
+      }))
+    },
+  });
 }
