1c30be63c652c7e57747d7e60ba7aabd7f1c9bd1
adqn <57331120+adqn@users.noreply.github.com>
2021-08-21 12:41:14 -0700

    experimenting with different tracking process

--- src/Pages/UserArea.tsx
+++ src/Pages/UserArea.tsx
@@ -1,8 +1,114 @@
 import React, { useState, useEffect } from 'react';
-// import { findRenderedComponentWithType } from 'react-dom/test-utils';
+import { useStopwatch} from 'react-timer-hook'
 import styled from 'styled-components'
 import customSelect from '../components/elements/CustomSelect'
 import "../App.css"
+
+const UserBarFlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid grey;
+  box-shadow: rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px;
+`
+
+const UserBarHeader = styled.div`
+  // flex: 1;
+  display: flex;
+  flex-direction: row;
+  height: 50px;
+  border: none;
+  border-bottom: 1px solid grey;
+  background: lightgrey;
+`
+
+const UserBarInputHeader = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  border: none;
+  border-bottom: 1px solid grey;
+`
+
+const UserBarInput = styled.input`
+  width: 90%;
+  line-height: 30px;
+  margin-left: 10px; 
+  padding-left: 10px;
+  border: none;
+  outline: none;
+  caret-color: transparent;
+
+  &:hover {
+    margin-left: 9px;
+    border: 1px solid grey;
+  }
+
+  &:focus {
+    caret-color: black;
+  }
+`
+
+const UserBarTimer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 50px;
+  padding: 8px;
+  border: none;
+`
+
+const UserBarInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 50px;
+  padding: 8px;
+  border: none;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+    height: fit-content;
+    align-items: start;
+    justify-content: left;
+  }
+`
+
+const UserBarInfoItem = styled.div<{ width?: string; minwidth?: string | undefined; justify?: string | undefined}>`
+  display: flex;
+  flex-direction: row;
+  justify-content: ${props => props.justify || "none"};
+  width: ${props => props.width};
+  min-width: ${props => props.minwidth};
+  // border: 1px solid;
+
+  @media (max-width: 700px) {
+    justify-content: left;
+  }
+`
+
+const StopwatchButton = styled.button<{background: string}>`
+  display: inline-block;
+  height: 30px;
+  color: white;
+  text-align: center;
+  padding: 7px;
+  border: none;
+  border-radius: 2px;
+  background: ${props => props.background};
+`
+
+const ElapsedTime = styled.div`
+  display: inline-block;
+  padding-right: 10px;
+  font-weight: 400;
+  font-size: 1.2em;
+
+  @media (max-width: 700px) {
+    font-weight: 200;
+    font-size: 1em;
+  }
+`
 
 const Select = styled.select`
   // display: none;
@@ -27,6 +133,37 @@ interface Project {
 
 interface Callbacks {
   [name: string]: React.Dispatch<React.SetStateAction<boolean>> | (() => void);
+}
+
+const Stopwatch = ({ setTotalTime }: { setTotalTime: (t:string) => void}) => {
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({autoStart: false})
+
+  return (
+    <div>
+      <ElapsedTime>
+        {hours < 10 ? "0" + hours : hours}:
+        {minutes < 10 ? "0" + minutes : minutes}:
+        {seconds < 10 ? "0" + seconds : seconds}
+      </ElapsedTime>
+      <StopwatchButton
+        background={isRunning ? "orange" : "#008CBA"}
+        onClick={isRunning ? pause : start}
+      >
+        {isRunning ? "STOP" : "START"}
+      </StopwatchButton>
+      {/* <button onClick={pause}>Pause</button>
+      <button onClick={() => reset()}>Reset</button> */}
+    </div>
+  )
 }
 
 const timeDropDown = (totalTime: number, timeUnit: string, setTime: (t:number) => void, parentTimeProp: number) => {
@@ -75,6 +212,43 @@ const projectsDropDown = (projects: Project[], setProject: (s:string) => void) =
 
 const SubmitStatus = ({ didSubmit }: { didSubmit: boolean }) =>
   didSubmit ? <div>Entry submitted!</div> : <div>Submitting...</div>
+
+const UserBar = ({current}: {current? : boolean | undefined}) => {
+  const [totalTime, setTotalTime] = useState("00:00:00")
+
+  return (
+    <UserBarFlexContainer>
+      {/* <UserBarHeader>
+      </UserBarHeader> */}
+      <UserBarInputHeader>
+        <UserBarInput
+          placeholder={"Current project"}
+        />
+      </UserBarInputHeader>
+      <UserBarTimer>
+        <UserBarInfoItem
+          width={"35%"}
+          justify={"center"}
+        >
+          Tags
+        </UserBarInfoItem>
+        <UserBarInfoItem
+          width={"35%"}
+          justify={"center"}
+        >
+          $
+        </UserBarInfoItem>
+        <UserBarInfoItem
+          width={"25%"}
+          minwidth={"130px"}
+          justify={"right"}
+        >
+          <Stopwatch setTotalTime={setTotalTime} />
+        </UserBarInfoItem>
+      </UserBarTimer>
+    </UserBarFlexContainer>
+  )
+}
 
 type FIXME = any;
 
@@ -147,6 +321,8 @@ export default function UserArea({callbacks}: {callbacks: Callbacks}) {
 
   return (
     <div>
+      <UserBar />
+      <br />
       {projects ?
         projectsDropDown(projects, setProject) :
         projectsDropDown(dummyProjects, setProject)}
