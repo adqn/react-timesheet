4c27d7c30aaad4b37179ed083abb42dc14f5c56d
adqn <57331120+adqn@users.noreply.github.com>
2021-09-27 16:39:15 -0700

    styling issue

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -12,7 +12,7 @@ const TopbarContainer = styled.div`
   height: 50px;
   background: white;
   box-shadow: rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px;
-  z-index: 1;
+  z-index: 2;
 `
 
 const TempMenuIndicator = styled.h1`
