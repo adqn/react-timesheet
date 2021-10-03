ae9873d0d8e89a10faa5c36ad0ef11ef925c185c
adqn <57331120+adqn@users.noreply.github.com>
2021-10-03 13:43:40 -0700

    naive task stopping implemented

--- api/api.js
+++ api/api.js
@@ -25,11 +25,11 @@ const tasks = []
 
 const startTask = (projectId, taskId, startTime) => { 
   const sqlInsertNewTask = `INSERT INTO tasks (task) VALUES ($1)`
+  const sqlGetLastTaskId = `SELECT task->'id' FROM tasks
+                            ORDER BY task->'id' DESC LIMIT 1`
   const sqlGetTasks = `SELECT task from tasks
                        WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
                        AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
-  const sqlGetLastTaskId = `SELECT task->'id' FROM tasks
-                            ORDER BY task->'id' DESC LIMIT 1`
   const sqlUpdateTask = `UPDATE tasks 
                          SET task = jsonb_set(task, '{start}', '${startTime}', false)
                          WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
@@ -62,9 +62,11 @@ const startTask = (projectId, taskId, startTime) => {
           })
         } else {
           // do resume task thing
+          // just create duplicate tasks with different start/end and combine durations?
+          // but then removing tasks is ambiguous...
           // const taskIdx = tasks.find(task => task.id === taskId && task.projectId === projectId)
           // tasks[foundTaskIdx].start = startTime
-          pool.query(sqlUpdateTask, (err, res) => {
+          pool.query(sqlUpdateTask, err => {
             if (err) throw err
             else console.log("Successfully updated task")
           })
@@ -74,9 +76,25 @@ const startTask = (projectId, taskId, startTime) => {
 }
 
 const stopTask = (projectId, taskId, endTime) => {
-  const foundTaskIdx = tasks.findIndex(task => task.id === taskId && task.projectId === projectId)
-  if (!foundTaskIdx) return
-  tasks[foundTaskIdx].end = endTime
+  const sqlGetTasks = `SELECT task from tasks
+                       WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
+                       AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
+  const sqlUpdateTask = `UPDATE tasks 
+                        SET task = jsonb_set(task, '{end}', '${endTime}', false)
+                        WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
+                        AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
+  pool.query(sqlGetTasks, (err, res) => {
+    if (err) return err
+    else {
+      if (res.rows.length === 0) return
+      else {
+        pool.query(sqlUpdateTask, err => {
+          if (err) throw err
+          else console.log("Successfully ended task")
+        })
+      }
+    }
+  })
 }
 
 const newTemplate = template => {
@@ -104,9 +122,8 @@ router.get("/tasks", (req, res) => {
 
 router.get("/starttask", (req, res) => {
   const currentTime = Date.now()
-  // let attrs = JSON.parse(req.body)
-  // startTask(attrs.projectId, attrs.timerId, currentTime)
-  startTask("1", "1", currentTime)
+  let attrs = JSON.parse(req.body)
+  startTask(attrs.projectId, attrs.timerId, currentTime)
 })
 
 router.get("/stoptask", (req, res) => {
