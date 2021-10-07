5e0dd7e03e6d59111d5c8745303036456e355b51
adqn <57331120+adqn@users.noreply.github.com>
2021-10-07 13:56:33 -0700

    robust task stopping implemented

--- api/api.js
+++ api/api.js
@@ -18,85 +18,94 @@ const pool = new Pool({
 //   description: string;
 // }
 
-const tasks = []
+// const psqlCreateNewTask = `INSERT INTO tasks (task) VALUES ($1)`
+// const psqlGetLastTaskId = `SELECT task->'id' FROM tasks
+//                           ORDER BY task->'id' DESC LIMIT 1`
+// const psqlGetTasks = `SELECT task from tasks
+//                       WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
+//                       AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
+// const psqlUpdateTask = `UPDATE tasks 
+//                         SET task = jsonb_set(task, '{start}', '${startTime}', false)
+//                         WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
+//                         AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
 
-const startTask = (projectId, taskId, startTime) => { 
-  const sqlInsertNewTask = `INSERT INTO tasks (task) VALUES ($1)`
-  const sqlGetLastTaskId = `SELECT task->'id' FROM tasks
-                            ORDER BY task->'id' DESC LIMIT 1`
-  const sqlGetTasks = `SELECT task from tasks
-                       WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
-                       AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
-  const sqlUpdateTask = `UPDATE tasks 
-                         SET task = jsonb_set(task, '{start}', '${startTime}', false)
-                         WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
-                         AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
-                      
-  // Check if we have the given task ID, if not then create new task
-  // Project ID should always be valid, however
-  pool.query(sqlGetTasks, (err, res) => {
-      if (err) throw err
-      else {
-        // console.log(res.rows)
-        if (res.rows.length === 0) {
-          pool.query(sqlGetLastTaskId, (err, res) => {
-            if (err) throw err
-            else {
-              const newTaskId = res.rows[0] ? (parseInt(res.rows[0]['?column?']) + 1).toString() : "1"
-              const newTask = {
-                id: newTaskId,
-                projectId,
-                start: startTime,
-                end: null,
-                description: null
-              }
+// set first task's end time to the start time of next task in queue
+// tasks go to queue before db update?
+let taskQueue = []
 
-              pool.query(sqlInsertNewTask, [JSON.stringify(newTask)], (err, res) => {
-                if (err) throw err
-                else console.log("Successfully created new task")
-              })
-            }
-          })
-        } else {
-          // TODO: start/end applicable to grouped tasks, add "duration" property
-          // requires duplicate records, which is OK in this case 
-          // total task time (but not duration) based on earliest and latest duplicate entries
-          // may need sub IDs? or delete tasks in groups based on timestamp?
-          pool.query(sqlUpdateTask, err => {
-            if (err) throw err
-            else console.log("Successfully updated task")
-          })
-        }
-      }
-    })
+const startTask = task => {
+  const foundTaskIdx = taskQueue.findIndex(foundTask => foundTask.id === task.id
+    && foundTask.projectId === task.projectId
+    && foundTask.start === task.start
+  )
+
+  if (foundTaskIdx === -1) {
+    // start new task and end first task in queue
+    taskQueue.push(task)
+    if (taskQueue.length > 1) {
+      taskQueue[0].end = task.start
+      stopTask(taskQueue[0])
+    }
+  } else stopTask(task)
+  console.log(taskQueue)
 }
 
-const stopTask = (projectId, taskId, endTime) => {
-  const sqlGetTasks = `SELECT task from tasks
-                       WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
-                       AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
-  const sqlUpdateTask = `UPDATE tasks 
-                        SET task = jsonb_set(task, '{end}', '${endTime}', false)
-                        WHERE CAST (task->>'id' AS INTEGER) = ${taskId}
-                        AND CAST (task->>'projectId' AS INTEGER) = ${projectId}`
-  pool.query(sqlGetTasks, (err, res) => {
-    if (err) return err
+const stopTask = task => {
+  const psqlCreateNewTask = `INSERT INTO tasks (task) VALUES ($1)`
+  const foundTaskIdx = taskQueue.findIndex(foundTask => foundTask.id === task.id
+    && foundTask.projectId === task.projectId
+    && foundTask.start === task.start
+  )
+
+  if (foundTaskIdx === -1) return
+
+  pool.query(psqlCreateNewTask, [JSON.stringify(task)], (err, res) => {
+    if (err) throw err
     else {
-      if (res.rows.length === 0) return
-      else {
-        pool.query(sqlUpdateTask, err => {
-          if (err) throw err
-          else console.log("Successfully ended task")
-        })
-      }
+      let tempTaskQueue = []
+      taskQueue.forEach((task, index) => { if (index !== foundTaskIdx) tempTaskQueue.push(task) })
+      taskQueue = tempTaskQueue
+      console.log("Successfully created new task")
     }
   })
 }
 
-const newTemplate = template => {
-  const sql = `INSERT INTO templates (template) VALUES ($1);`
+const updateTask = task => {
+  const psqlUpdateTaskDescriptionByTime = `UPDATE tasks 
+                        SET task = jsonb_set(task, '{description}', '${task.description}', false)
+                        WHERE task->>'start' = ${task.start}`
+  const psqlUpdateTaskDescriptionById = `UPDATE tasks 
+                        SET task = jsonb_set(task, '{description}', '${task.description}', false)
+                        WHERE CAST (task->>'id' AS INTEGER) = ${task.id}`
+  const psqlUpdateTaskDescriptionByProjectId = `UPDATE tasks 
+                        SET task = jsonb_set(task, '{description}', '${task.description}', false)
+                        WHERE CAST (task->>'projectId' AS INTEGER) = ${task.projectId}`
+  const psqlUpdateTaskByProjectId = `UPDATE tasks 
+                        SET task = jsonb_set(task, '{id}', '${task.id}', false)
+                        WHERE CAST (task->>'projectId' AS INTEGER) = {task.projectId}`
+  const psqlUpdateTaskById = `UPDATE tasks 
+                        SET task = jsonb_set(task, '{projectId}', '${task.projectId}', false)
+                        WHERE CAST (task->>'id' AS INTEGER) = {task.id}`
 
-  pool.query(sql, [template], err => {
+  if (!task.id) {
+    if (!task.projectId) {
+      pool.query(psqlUpdateTaskDescriptionByTime, (err, res) => {
+        if (err) console.log(err)
+        else console.log("Successfully updated task description")
+      })
+    } else {
+      pool.query(psqlUpdateTaskByProjectId, (err, res) => {
+        if (err) console.log(err)
+        else console.log("Successfully updated task description")
+      })
+    }
+  }
+}
+
+const newTemplate = template => {
+  const psql = `INSERT INTO templates (template) VALUES ($1);`
+
+  pool.query(psql, [template], err => {
     if (err) throw err
   })
 }
@@ -114,21 +123,30 @@ router.get("/tasks", (req, res) => {
 })
 
 router.post("/starttask", (req, res) => {
-  const currentTime = Date.now()
-  let attrs = req.body
-  startTask(attrs.projectId, attrs.taskId, currentTime)
+  if (taskQueue.length <= 1) {
+    const currentTime = Date.now()
+    startTask(req.body)
+  } else {
+    for (let task of taskQueue) stopTask(task)
+    res.send("clearTaskQueue")
+  }
 })
 
 router.post("/stoptask", (req, res) => {
   const currentTime = Date.now()
   let attrs = req.body
-  stopTask(attrs.projectId, attrs.taskId, currentTime)
+  stopTask(req.body)
+  res.sendStatus(200)
+})
+
+router.post("/updatetask", (req, res) => {
+  updateTask(req.body)
 })
 
 router.get("/templates", (req, res) => {
-  const sql = `SELECT * FROM templates;`
+  const psql = `SELECT * FROM templates;`
 
-  pool.query(sql, (err, rows) => {
+  pool.query(psql, (err, rows) => {
     if (err) throw err;
     res.send(rows.rows)
   })
