45a6a5a9af823bda2db74a7a008a54c84404a5e0
Gregory Ksionda <ksiondag846@gmail.com>
2022-02-12 18:03:25 -0800

    Removing old timeblock code (#6)

--- src/Pages/Timeblock/Timeblock.styled.tsx
+++ src/Pages/Timeblock/Timeblock.styled.tsx
@@ -1,134 +1,43 @@
-import styled, { css } from 'styled-components'
-import 'antd/dist/antd.css'
+import styled from "styled-components";
+import { Table, Input, Menu } from "antd";
+import "antd/dist/antd.css";
 
-export const cellWidth: number = 155 
-export const cellHeight: number = 36
-export const headerHeight: number = 32
+import { TimeblockTableProps } from "./types";
+import React from "react";
 
-export const ModalDialog = styled.div`
-  position: absolute;
-  top: 25%;
-  left: 40%;
-  // height: 300px;
-  // width: 300px;
-  // height: fit-content;
-  // width: fit-content
-  border-radius: 7px;
-  background: white;
-`
-
-export const FlexContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  // flex-flow: column wrap;
-  padding-left: 6px;
-  // min-width: 50%;
-  // width: fit-content;
-  // height: fit-content;
-  // border: 1px solid black;
-`
-
-export const ControlContainer = styled.div`
-  display: flex;
-  width: fit-content;
+export const Cell = styled.div<{ selected?: boolean }>`
+  padding: 16px;
   height: 100%;
-  // border: 1px solid;
-`
-
-interface CellStyles {
-  width: number;
-  omitLeftBorder: boolean;
-  omitRightBorder: boolean;
-}
-
-export const Cell = styled.div<CellStyles>`
-  flex: 1;
-  flex-grow: 1;
-  // flex-shrink: 0;
-  line-height: 20px;
-  font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
-  font-size: 14px;
-  width: ${props => props.width};
-  min-width: 150px;
-  max-width: 150px;
-  min-height: 20px;
-  // min-width: 100px;
-  padding: 7px;
-  // padding-left: 10px;
-  // padding-right: 10px;
-  margin-left: -1px;
-  border: 1px solid lightgrey;
-  border-left: ${props => props.omitLeftBorder ? "none" : "1px solid lightgrey"};
-  border-right: ${props => props.omitRightBorder ? "none" : "1px solid lightgrey"};
-  overflow-wrap: anywhere;
-  user-select: none; 
-  -webkit-user-select: none; 
-  -khtml-user-select: none; 
-  -moz-user-select: none; 
-  -ms-user-select: none; 
+  width: 100%;
+  background: ${(props) => props.selected ? "rgb(51, 204, 255, .25)" : "white"};
+  border: ${(props) => props.selected ? "solid 1px #0099ff" : "none"};
   &:hover {
     cursor: default;
   }
-`
+`;
 
-export const HeaderCell = styled.div<CellStyles>`
-  flex: 1;
-  flex-grow: 1;
-  line-height: 30px;
-  font-size: .9em;
-  font-weight: 100;
-  text-align: left;
-  padding-left: 10px;
-  color: grey;
-  height: 32px;
-  // width: 150px;
-  width: ${props => props.width};
-  // min-width: fit-content;
-  // min-width: 50px;
-  margin-left: -1px;
-  border: solid 1px lightgrey;
-  border-left: ${props => props.omitLeftBorder ? "none" : "1px solid lightgrey"};
-  border-right: ${props => props.omitRightBorder ? "none" : "1px solid lightgrey"};
-  user-select: none; 
-  -webkit-user-select: none; 
-  -khtml-user-select: none; 
-  -moz-user-select: none; 
-  -ms-user-select: none; 
+export const EditCell = styled(Input.TextArea)<{}>`
+  width: 100%;
+  height: 100%;
+  word-wrap: break-word;
+  word-break: break-all;
   &:hover {
-    cursor: default;
-    background: #F1F1F1;
+    // cursor: default;
   }
-`
+`;
 
-// Crazy offsets that I'm not sure will work on every device???
-export const SelectedCell = styled.div`
-  position: absolute;
-  top: ${props => props.top + "px"};
-  left: ${props => props.left + "px"};
-  height: ${props => props.height - 2 + "px"};
-  width: ${props => props.width - 1 + "px"};
-  border: solid 1px #0099ff;
-  background: rgb(51, 204, 255, .25);
-  // opacity: .3;
-  z-index: 1;
-  pointer-events: none;
-`
-
-export const Row = styled.div`
-  display: flex;
-  // width: fit-content;
-  // min-width: 420px;
-  // width: 100%;
-  min-height: 30px;
-  margin-left: -1px;
-  margin-top: -1px;
-  // border: 1px solid;
-`
+export const NewTable = styled<React.ComponentType<TimeblockTableProps>>(Table)`
+  .ant-table-cell {
+    padding: 0;
+    width: 20%;
+    min-height: 60px;
+  }
+`;
 
 export const AddRemoveButton = styled.div`
   display: inline-block;
   height: fit-content;
-  width: 20px;
+  width: 25px;
   text-align: center;
   font: 1.5em bold;
   font-weight: 900;
@@ -141,113 +50,36 @@ export const AddRemoveButton = styled.div`
   -webkit-user-select: none;
   -o-user-select: none;
   &:hover {
-    background: #F4F4F4;
+    background: #f4f4f4;
     cursor: pointer;
   }
-`
+`;
 
-export const ColumnResizeContainer = styled.div`
-  position: absolute;
-  margin-top: 6px;
-  left: ${props => props.left + "px"}
-`
-
-export const ColumnResizeBar = css`
-  -webkit-appearance: none;
-  visibility: visible;
-  width: 7px;
-  height: 34px;
-  margin-top: -1px;
-  // margin-left: -12px;
-  border: none;
-  background: #27B7FF;
-  opacity: 1;
-  z-index: 1;
+export const DefaultButton = styled.div`
+  border: 1px solid lightgrey;
+  height: 30px;
+  width: fit-content;
+  padding: 2px;
+  padding-left: 5px;
+  padding-right: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+  user-select: none;
   &:hover {
-    cursor: col-resize;
+    cursor: pointer;
+  }
+  &:active {
+    background: #f2f2f2;
   }
 `
 
-export const ColumnResizeSlider = styled.input`
-&,
-&::-webkit-slider-thumb {
-  -webkit-appearance: none;
-}
-&::-webkit-slider-thumb {
-  ${ColumnResizeBar};
-}
-
-&::-moz-range-thumb {
-  ${ColumnResizeBar};
-}
-
-&::-ms-thumb {
-  ${ColumnResizeBar};
-}
-
-&::-ms-tooltip {
-  display: none;
-}
-
-&::-moz-focus-outer {
-  border: 0;
-}
+export const InputField = styled(Input)`
+  width: 200px;
 `
 
-// export const cellSelectedOverlay = styled.div`
-//   // visibility: hidden;
-//   position: absolute;
-//   width: ${props => props.width}
-//   height: ${props => props.height}
-//   border: 2px solid lightblue;
-// `
-
-export const CellLayer = styled.div`
-  display: inline-block;
-  width: 100%;
-  height: 100%;
-  // border: 1px solid lightblue;
-  border: none;
-  // margin-left: -1px;
-  // margin-top: -1px; 
-  background: white;
-  -webkit-appearance: none;
-  -moz-appearance: none;
+export const DefaultMenu = styled(Menu)`
+  width: 200px;
 `
 
-interface EditCellStyles {
-  visibility: string;
-  left: string;
-  top: string;
-  width: string;
-  minheight: string;
-  height: string;
-}
-
-export const EditCell = styled.textarea<EditCellStyles>`
-  visibility: ${props => props.visibility};
-  position: absolute;
-  left: ${props => props.left};
-  top: ${props => props.top};
-  font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
-  font-size: 14px;
-  // font-weight: 60;
-  line-height: 20px;
-  width: ${props => props.width};
-  min-height: ${props => props.minheight};
-  height: ${props => props.height};
-  padding: 4px;
-  white-space: pre-wrap;
-  word-break: break-word;
-  // margin-top: 2px;
-  // margin-left: 2px;
-  box-sizing: border-box;
-  // border: solid 1px grey;
-  border: none;
-  border-radius: 3px;
-  box-shadow: rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px;
-  background: white;
-  overflow: hidden;
-  outline: none;
-  resize: none;
+export const MenuItem = styled(Menu.Item)`
 `
